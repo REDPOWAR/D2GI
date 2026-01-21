763771f94fe0187abc856208aea2a75a642ef783
Silent <zdanio95@gmail.com>
2026-01-21 14:44:03 +0100

    Simplify the blitter code, and make it SM2.0-friendly

--- D2GI/src/d2gi/d2gi_blitter.cpp
+++ D2GI/src/d2gi/d2gi_blitter.cpp
@@ -12,35 +12,21 @@
 
 using namespace D3D9;
 
-
-D2GIBlitter::D2GIBlitter(D2GI* pD2GI)
-	: D2GIBase(pD2GI), m_pVDecl(NULL), m_pVB(NULL), m_pVS(NULL), m_pPS(NULL)
+void D2GIBlitter::ReleaseResource()
 {
-
+	m_pVB.Reset();
+	m_pVDecl.Reset();
+	m_pVS.Reset();
+	m_pPS.Reset();
 }
 
 
-D2GIBlitter::~D2GIBlitter()
+void D2GIBlitter::LoadResource()
 {
-	ReleaseResource();
-}
-
-
-VOID D2GIBlitter::ReleaseResource()
-{
-	RELEASE(m_pVB);
-	RELEASE(m_pVDecl);
-	RELEASE(m_pVS);
-	RELEASE(m_pPS);
-}
-
-
-VOID D2GIBlitter::LoadResource()
-{
-	D3DVERTEXELEMENT9 asVertexElements[] =
+	const D3DVERTEXELEMENT9 asVertexElements[] =
 	{
-		{0, 0, D3DDECLTYPE_FLOAT3, D3DDECLMETHOD_DEFAULT, D3DDECLUSAGE_POSITION, 0},
-		{0, 12, D3DDECLTYPE_FLOAT2, D3DDECLMETHOD_DEFAULT, D3DDECLUSAGE_TEXCOORD, 0},
+		{0, 0, D3DDECLTYPE_FLOAT2, D3DDECLMETHOD_DEFAULT, D3DDECLUSAGE_POSITION, 0},
+		{0, 8, D3DDECLTYPE_FLOAT2, D3DDECLMETHOD_DEFAULT, D3DDECLUSAGE_TEXCOORD, 0},
 		D3DDECL_END()
 	};
 	IDirect3DDevice9* pDev = GetD3D9Device();
@@ -48,17 +34,15 @@ VOID D2GIBlitter::LoadResource()
 	if (FAILED(pDev->CreateVertexDeclaration(asVertexElements, &m_pVDecl)))
 		Logger::Error(TEXT("Failed to create blitter vertex declaration"));
 
-	FLOAT afVerts[] =
+	const FLOAT afVerts[] =
 	{
-		-0.5f, 0.5f, 0.0,  0.0, 0.0,
-		0.5f, 0.5f, 0.0,  1.0f, 0.0,
-		-0.5f, -0.5f, 0.0,  0.0, 1.0f,
+		-0.5f, 0.5f,  0.0, 0.0,
+		0.5f, 0.5f,  1.0f, 0.0,
+		-0.5f, -0.5f,  0.0, 1.0f,
 
-		0.5f, 0.5f, 0.0,  1.0f, 0.0,
-		0.5f, -0.5f, 0.0,  1.0f, 1.0f,
-		-0.5f, -0.5f, 0.0, 0.0, 1.0f,
+		0.5f, -0.5f,  1.0f, 1.0f,
 	};
-	VOID* pData;
+	void* pData;
 
 	if (FAILED(pDev->CreateVertexBuffer(sizeof(afVerts), D3DUSAGE_WRITEONLY, 0, D3DPOOL_DEFAULT, &m_pVB, NULL)))
 		Logger::Error(TEXT("Failed to create blitter vertex buffer"));
@@ -77,52 +61,56 @@ VOID D2GIBlitter::LoadResource()
 		Logger::Error(TEXT("Failed to create blitter pixel shader"));
 }
 
-
-VOID D2GIBlitter::Blit(IDirect3DSurface9* pDst, FRECT* pDstRT,
-	IDirect3DTexture9* pSrc, FRECT* pSrcRT, BOOL bEmulateCK)
+void D2GIBlitter::Blit(IDirect3DSurface9* pDst, const FRECT* pDstRT,
+	IDirect3DTexture9* pSrc, const FRECT* pSrcRT, bool bEmulateCK)
 {
 	IDirect3DDevice9* pDev = GetD3D9Device();
 
 	struct
 	{
 		float x, y, z, w;
-	} vTextureRect, vScreenPosRect, vTextureBorder;
+	} vsConstants[2], psConstants[1];
+
+	auto& vScreenPosRect = vsConstants[0];
+	auto& vTextureRect = vsConstants[1];
+	auto& vTextureBorder = psConstants[0];
 
 	D3DSURFACE_DESC sDstDesc, sSrcDesc;
 	FRECT rtSrc, rtDst;
 
-	IDirect3DSurface9* pOriginalRT = NULL, * pOriginalDS;
-	IDirect3DBaseTexture9* pCurrentTexture1 = NULL, * pCurrentTexture2 = NULL;
+	Microsoft::WRL::ComPtr<IDirect3DSurface9> pOriginalRT;
+	Microsoft::WRL::ComPtr<IDirect3DBaseTexture9> pCurrentTexture;
+
 	DWORD dwMinFilter, dwMagFilter, dwCullMode, dwAlphaTestEnable;
 	DWORD dwAlphaBlending, dwAlphaOp, dwAlphaSrc, dwAlphaDst;
 	DWORD dwZEnable, dwZWriteEnable;
+	DWORD dwFogEnable;
 	D3DVIEWPORT9 sOriginalVP, sUsedVP;
 
 	pDst->GetDesc(&sDstDesc);
 	pSrc->GetLevelDesc(0, &sSrcDesc);
 
-	if (pSrcRT != NULL)
+	if (pSrcRT != nullptr)
 		rtSrc = *pSrcRT;
 	else
 		rtSrc = FRECT(0.0, 0.0, (FLOAT)sSrcDesc.Width, (FLOAT)sSrcDesc.Height);
 
-	if (pDstRT != NULL)
+	if (pDstRT != nullptr)
 		rtDst = *pDstRT;
 	else
 		rtDst = FRECT(0.0, 0.0, (FLOAT)sDstDesc.Width, (FLOAT)sDstDesc.Height);
 
-	pDev->GetRenderTarget(0, &pOriginalRT);
-	pDev->GetDepthStencilSurface(&pOriginalDS);
+	pDev->GetRenderTarget(0, pOriginalRT.GetAddressOf());
 	pDev->GetViewport(&sOriginalVP);
 	pDev->GetRenderState(D3DRS_ZENABLE, &dwZEnable);
 	pDev->GetRenderState(D3DRS_ZWRITEENABLE, &dwZWriteEnable);
-	pDev->GetTexture(0, &pCurrentTexture1);
-	pDev->GetTexture(1, &pCurrentTexture2);
+	pDev->GetTexture(0, pCurrentTexture.GetAddressOf());
 	pDev->GetSamplerState(0, D3DSAMP_MINFILTER, &dwMinFilter);
 	pDev->GetSamplerState(0, D3DSAMP_MAGFILTER, &dwMagFilter);
 	pDev->GetRenderState(D3DRS_CULLMODE, &dwCullMode);
 	pDev->GetRenderState(D3DRS_ALPHABLENDENABLE, &dwAlphaBlending);
 	pDev->GetRenderState(D3D9::D3DRS_ALPHATESTENABLE, &dwAlphaTestEnable);
+	pDev->GetRenderState(D3D9::D3DRS_FOGENABLE, &dwFogEnable);
 	if (bEmulateCK)
 	{
 		pDev->GetRenderState(D3D9::D3DRS_BLENDOP, &dwAlphaOp);
@@ -142,8 +130,12 @@ VOID D2GIBlitter::Blit(IDirect3DSurface9* pDst, FRECT* pDstRT,
 
 	vScreenPosRect.x = 2.0f * rtDst.GetWidth() / (FLOAT)sDstDesc.Width;
 	vScreenPosRect.y = 2.0f * rtDst.GetHeight() / (FLOAT)sDstDesc.Height;
-	vScreenPosRect.z = 2.0f * (-0.5f + rtDst.GetXCenter()) / (FLOAT)sDstDesc.Width - 1.0f;
-	vScreenPosRect.w = -2.0f * (-0.5f + rtDst.GetYCenter()) / (FLOAT)sDstDesc.Height + 1.0f;
+
+	// Calculate the rectangle position in clip space [-1, 1],
+	// shifted by half the viewport unit to account for the half-pixel offset.
+	// 2.0 * (rtDst.Center() - 0.5) / sDstDesc + 1.0
+	vScreenPosRect.z = (-1.0f + rtDst.fLeft + rtDst.fRight) / (FLOAT)sDstDesc.Width - 1.0f;
+	vScreenPosRect.w = -(-1.0f + rtDst.fTop + rtDst.fBottom) / (FLOAT)sDstDesc.Height + 1.0f;
 
 	sUsedVP.X = 0;
 	sUsedVP.Y = 0;
@@ -152,24 +144,20 @@ VOID D2GIBlitter::Blit(IDirect3DSurface9* pDst, FRECT* pDstRT,
 	sUsedVP.MinZ = 0.0;
 	sUsedVP.MaxZ = 1.0f;
 
-	if (pDst != pOriginalRT)
+	if (pDst != pOriginalRT.Get())
 		pDev->SetRenderTarget(0, pDst);
-	pDev->SetDepthStencilSurface(NULL);
 	pDev->SetViewport(&sUsedVP);
 
-	pDev->SetVertexDeclaration(m_pVDecl);
-	pDev->SetStreamSource(0, m_pVB, 0, sizeof(FLOAT) * 5);
+	pDev->SetVertexDeclaration(m_pVDecl.Get());
+	pDev->SetStreamSource(0, m_pVB.Get(), 0, sizeof(FLOAT) * 4);
 
-	pDev->SetVertexShader(m_pVS);
-	pDev->SetVertexShaderConstantF(0, &vScreenPosRect.x, 1);
-	pDev->SetVertexShaderConstantF(1, &vTextureRect.x, 1);
+	pDev->SetVertexShader(m_pVS.Get());
+	pDev->SetVertexShaderConstantF(0, reinterpret_cast<float*>(vsConstants), std::size(vsConstants));
 
-	pDev->SetPixelShader(m_pPS);
-	pDev->SetPixelShaderConstantF(0, &vTextureBorder.x, 1);
-
+	pDev->SetPixelShader(m_pPS.Get());
+	pDev->SetPixelShaderConstantF(0, reinterpret_cast<float*>(psConstants), std::size(psConstants));
 
 	pDev->SetTexture(0, pSrc);
-	pDev->SetTexture(1, NULL);
 	pDev->SetSamplerState(0, D3DSAMP_MINFILTER, D3DTEXF_LINEAR);
 	pDev->SetSamplerState(0, D3DSAMP_MAGFILTER, D3DTEXF_LINEAR);
 	pDev->SetRenderState(D3DRS_CULLMODE, D3DCULL_NONE);
@@ -177,6 +165,7 @@ VOID D2GIBlitter::Blit(IDirect3DSurface9* pDst, FRECT* pDstRT,
 	pDev->SetRenderState(D3DRS_ZENABLE, FALSE);
 	pDev->SetRenderState(D3DRS_ZWRITEENABLE, FALSE);
 	pDev->SetRenderState(D3DRS_ALPHATESTENABLE, FALSE);
+	pDev->SetRenderState(D3DRS_FOGENABLE, FALSE);
 	if (bEmulateCK)
 	{
 		pDev->SetRenderState(D3DRS_ALPHABLENDENABLE, TRUE);
@@ -187,14 +176,11 @@ VOID D2GIBlitter::Blit(IDirect3DSurface9* pDst, FRECT* pDstRT,
 	else
 		pDev->SetRenderState(D3DRS_ALPHABLENDENABLE, FALSE);
 
-	pDev->DrawPrimitive(D3DPT_TRIANGLELIST, 0, 2);
+	pDev->DrawPrimitive(D3DPT_TRIANGLESTRIP, 0, 2);
 
 	pDev->SetPixelShader(NULL);
 	pDev->SetVertexShader(NULL);
-	pDev->SetTexture(0, pCurrentTexture1);
-	pDev->SetTexture(1, pCurrentTexture2);
-	RELEASE(pCurrentTexture1);
-	RELEASE(pCurrentTexture2);
+	pDev->SetTexture(0, pCurrentTexture.Get());
 	pDev->SetViewport(&sOriginalVP);
 	pDev->SetSamplerState(0, D3DSAMP_MINFILTER, dwMinFilter);
 	pDev->SetSamplerState(0, D3DSAMP_MAGFILTER, dwMagFilter);
@@ -202,16 +188,14 @@ VOID D2GIBlitter::Blit(IDirect3DSurface9* pDst, FRECT* pDstRT,
 	pDev->SetRenderState(D3DRS_ZWRITEENABLE, dwZWriteEnable);
 	pDev->SetRenderState(D3DRS_CULLMODE, dwCullMode);
 	pDev->SetRenderState(D3DRS_ALPHABLENDENABLE, dwAlphaBlending);
-	pDev->SetRenderState(D3D9::D3DRS_ALPHATESTENABLE, dwAlphaTestEnable);
+	pDev->SetRenderState(D3DRS_ALPHATESTENABLE, dwAlphaTestEnable);
+	pDev->SetRenderState(D3DRS_FOGENABLE, dwFogEnable);
 	if (bEmulateCK)
 	{
 		pDev->SetRenderState(D3DRS_BLENDOP, dwAlphaOp);
 		pDev->SetRenderState(D3DRS_SRCBLEND, dwAlphaSrc);
 		pDev->SetRenderState(D3DRS_DESTBLEND, dwAlphaDst);
 	}
-	if (pDst != pOriginalRT)
-		pDev->SetRenderTarget(0, pOriginalRT);
-	pDev->SetDepthStencilSurface(pOriginalDS);
-	RELEASE(pOriginalDS);
-	RELEASE(pOriginalRT);
+	if (pDst != pOriginalRT.Get())
+		pDev->SetRenderTarget(0, pOriginalRT.Get());
 }
